48e43370c8eec51cd54e44681c7796d4f6135bf9
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 14:37:15 +0200

    Add method index in the api project controller to return all the projects in the database

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,7 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Project;
+
 class ProjectController extends Controller
 {
-    //
+    /**
+     * Return a json response with all the project saved in the local db (paginated 5 by 5)
+     */
+    public function index()
+    {
+        //Eager loading: return also the category data
+        $projects = Project::with("category")->paginate(5);
+
+        if($projects){
+            return response()->json([
+                "success" => true,
+                "results" => $projects
+            ]);
+        }else{
+            return response()->json([
+                "success" => false,
+                "results" => "Projects not found!"
+            ]);
+        }
+    }
 }
