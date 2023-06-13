4b130c91c4a71812d0c79c7c69c0e0cc57ab430e
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 14:40:36 +0200

    Fix api routes

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use App\Http\Controllers\Api\ProjectController;
 */
 
 Route::get("/projects", [ProjectController::class, 'index']);
-Route::get("/projects/{slug}", [BrandController::class, 'show']);
+Route::get("/projects/{slug}", [ProjectController::class, 'show']);
