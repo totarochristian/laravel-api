b20a20706dc92bc0884e05ac897d54486c10fd2d
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 14:39:57 +0200

    Update the api routes to let users retrieve the data of a specified project

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use App\Http\Controllers\Api\ProjectController;
 */
 
 Route::get("/projects", [ProjectController::class, 'index']);
+Route::get("/projects/{slug}", [BrandController::class, 'show']);
